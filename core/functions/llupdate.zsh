5a5f204bafaa4e1ac13d32ffa7ee88a375029560
wujibear <hello@wujibear.studio>
2021-05-26 11:33:13 -0700

    allow llupdate to not change dirs

--- core/functions/llupdate.zsh
+++ core/functions/llupdate.zsh
@@ -1,7 +1,9 @@
 # updates LLOCAL by pulling the latest from the repo
 function llupdate() {
+  current_dir=$PWD
   cd $LLOCAL_DIR
   llcolor $YELLOW "Fetching the latest version of llocal..."
   git pull origin main
+  cd $current_dir
   llsource
 }
